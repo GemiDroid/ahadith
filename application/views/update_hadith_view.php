c36c2e08d513d10e715654845ae9a3a54a63f2a0
sadafqazi <sadaf_qazi93@yahoo.com>
2015-05-25 13:16:25 +0500

    Corrected Hadith update view in admin panel

--- application/views/update_hadith_view.php
+++ application/views/update_hadith_view.php
@@ -1,68 +1,67 @@
-
-            <div class="col-md-9" style="margin-top: 50px;">
+<div class="col-md-9" style="margin-top: 50px;">
 
   <?php if(!empty($hadith)): ?>
   <h4>Updating Hadith!</h4>
   <hr/>
 
+  
       <?php echo form_open('admin/hadith/update/'.$hadith_id);?>
-      
       
       <div class="control-group form-inline">
         <label class="control-label" for="txt_plain_ar">Plain Arabic:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <textarea class="form-control" name="txt_plain_ar" id="txt_plain_ar" value="<?php echo set_value('txt_plain_ar', (!empty($hadith) ? $hadith[0]->hadith_plain_ar : '')); ?>" style="width: 500px; height: 100px;" ></textarea>
+        <textarea class="form-control" name="txt_plain_ar" id="txt_plain_ar" style="width: 800px; height: 100px;" ><?php echo set_value('txt_plain_ar', (!empty($hadith) ? $hadith[0]->hadith_plain_ar : '')); ?>  </textarea>
         <div class="help-inline">
           <?php echo form_error('txt_plain_ar', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
+    </div>
       <br/>
     <div class="control-group form-inline">
         <label class="control-label" for="txt_plain_en">Plain English:</label>&nbsp;&nbsp;&nbsp;
-        <textarea class="form-control" name="txt_plain_en" id="txt_plain_en" value="<?php echo set_value('txt_plain_en', (!empty($hadith) ? $hadith[0]->hadith_plain_en : '')); ?>" style="width: 500px; height: 100px;"></textarea>
+        <textarea class="form-control" name="txt_plain_en" id="txt_plain_en" style="width: 800px; height: 100px;"><?php echo set_value('txt_plain_en', (!empty($hadith) ? $hadith[0]->hadith_plain_en : '')); ?></textarea>
         <div class="help-inline">
           <?php echo form_error('txt_plain_en', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
+    </div>
     <br/>
      <div class="control-group form-inline">
         <label class="control-label" for="txt_plain_ur">Plain Urdu:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <textarea class="form-control" name="txt_plain_ur" id="txt_plain_ur" value="<?php echo set_value('txt_plain_ur', (!empty($hadith) ? $hadith[0]->hadith_plain_ur : '')); ?>" style="width: 500px; height: 100px;"></textarea>
+        <textarea class="form-control" name="txt_plain_ur" id="txt_plain_ur" style="width: 800px; height: 100px;" ><?php echo set_value('txt_plain_ur', (!empty($hadith) ? $hadith[0]->hadith_plain_ur : '')); ?></textarea>
         <div class="help-inline">
           <?php echo form_error('txt_plain_ur', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
+    </div>
      <br/>
     <div class="control-group form-inline">
         <label class="control-label" for="txt_marked_ar">Marked Arabic:</label>&nbsp;&nbsp
-        <textarea class="form-control" name="txt_marked_ar" id="txt_marked_ar" value="<?php echo set_value('txt_marked_ar', (!empty($hadith) ? $hadith[0]->hadith_marked_ar : '')); ?>" style="width: 500px; height: 100px;"></textarea>
+        <textarea class="form-control" name="txt_marked_ar" id="txt_marked_ar"  style="width: 800px; height: 100px;"><?php echo set_value('txt_marked_ar', (!empty($hadith) ? $hadith[0]->hadith_marked_ar : '')); ?></textarea>
         <div class="help-inline">
           <?php echo form_error('txt_marked_ar', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
+    </div>
     <br/>
-  <div class="control-group form-inline">
+    <div class="control-group form-inline">
         <label class="control-label" for="txt_marked_en">Marked English:</label>
-        <textarea class="form-control" name="txt_marked_en" id="txt_marked_en" value="<?php echo set_value('txt_marked_en', (!empty($hadith) ? $hadith[0]->hadith_marked_en : '')); ?>" style="width: 500px; height: 100px;"></textarea>
+        <textarea class="form-control" name="txt_marked_en" id="txt_marked_en" style="width: 800px; height: 100px;"><?php echo set_value('txt_marked_en', (!empty($hadith) ? $hadith[0]->hadith_marked_en : '')); ?></textarea>
         <div class="help-inline">
           <?php echo form_error('txt_marked_en', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
-  <br/>
+    </div>
+    <br/>
     <div class="control-group form-inline">
         <label class="control-label" for="txt_marked_ur">Marked Urdu:</label>&nbsp;&nbsp;&nbsp;&nbsp;
-        <textarea class="form-control" name="txt_marked_ur" id="txt_marked_ur" value="<?php echo set_value('txt_marked_ur', (!empty($hadith) ? $hadith[0]->hadith_marked_ur : '')); ?>" style="width: 500px; height: 100px;" ></textarea>
+        <textarea class="form-control" name="txt_marked_ur" id="txt_marked_ur" style="width: 800px; height: 100px;"><?php echo set_value('txt_marked_ur', (!empty($hadith) ? $hadith[0]->hadith_marked_ur : '')); ?></textarea>
         <div class="help-inline">
           <?php echo form_error('txt_marked_ur', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
+    </div>
     <br/>
-  <div class="control-group form-inline">
+    <div class="control-group form-inline">
         <label class="control-label" for="txt_raw_ar">Raw Arabic:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <textarea class="form-control" name="txt_raw_ar" id="txt_raw_ar" value="<?php echo set_value('txt_raw_ar', (!empty($hadith) ? $hadith[0]->hadith_raw_ar : '')); ?>" style="width: 500px; height: 100px;"></textarea>
+        <textarea class="form-control" name="txt_raw_ar" id="txt_raw_ar" style="width: 800px; height: 100px;" ><?php echo set_value('txt_raw_ar', (!empty($hadith) ? $hadith[0]->hadith_raw_ar : '')); ?></textarea>
         <div class="help-inline">
           <?php echo form_error('txt_raw_ar', '<span class="text-error">', '</span>'); ?>
         </div>
-  </div>
+    </div>
      <br/>
     <div class="control-group form-inline">
         <label class="control-label" for="authenticity_id">Authenticity:</label>&nbsp;&nbsp;&nbsp;&nbsp;
@@ -77,7 +76,7 @@
 			
 	  <?php endif; ?>
     </div>
-<br/>
+    <br/>
     <div class="control-group form-inline">
      <input  type="submit" id="mysubmit" name="mysubmit" value="Update" class="btn btn-success"/>
         <a href="<?php echo base_url().'admin/hadith' ?>"><input type="button" value="Cancel" class="btn btn-primary"></a>
@@ -90,5 +89,5 @@
      echo 'Hadith doesnot exist';
      endif;
  ?>
-            </div>
-            </div>
+</div>
+</div>
